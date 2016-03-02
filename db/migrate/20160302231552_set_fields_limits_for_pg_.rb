e17bca6df52e177b83d79dbcba24401e12641133
Luca Melis <lucamelis12@gmail.com>
2016-03-02 23:19:43 +0000

    migration for fields limit

---
+++ db/migrate/20160302231552_set_fields_limits_for_pg_.rb
@@ -0,0 +1,4 @@
+class SetFieldsLimitsForPg < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160302225908) do
+ActiveRecord::Schema.define(version: 20160302231552) do
 
   create_table "calls", force: :cascade do |t|
     t.integer  "call_id",        limit: 8
